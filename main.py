8558898ba0f7e79e426dea2a01731c8de05c53c4
realjoerogan <76176911+realjoerogan@users.noreply.github.com>
2022-09-01 21:08:31 -0400

    added support for multiple gsi

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ music = DiscordUtils.Music()
 import logging
 import random
 import requests
-bot = commands.Bot(command_prefix='.')
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='.', intents=intents)
 
 
 global symbol1
@@ -19,12 +23,14 @@ global name2
 global turn
 turn = 0
 
+roundEnd = 0
 matchEnd = 0
 t_name2 = "T's"
 ct_name2 = "CT's"
 
 
 logger = logging.getLogger(__name__)
+
 
 class DurationConverter(commands.Converter):
     async def convert(self, ctx, argument):
@@ -36,19 +42,26 @@ class DurationConverter(commands.Converter):
 
         raise commands.BadArgument(message='Not a valid duration')
 
+
 def read_token():
     with open("token.txt", "r") as f:
         lines = f.readlines()
         return lines[0].strip()
 
+
 token = read_token()
-intents = discord.Intents.default()
-intents.members = True
 client = discord.Client(intents=intents)
 
 
 @bot.event
 async def on_ready():
+    # channel = bot.get_channel(1014279089022062612)
+    # embed = discord.Embed(
+    #     title=f"update: v0.9.8",
+    #     description=f"**whats new?**\nsupport for multiple game state integrations has been added, this means that anyone running the gsi in their config directly will have their matches processed through the bot. whoever starts their match first will have their match displayed in the nickname and status, and once the match ends/they leave the match that was second in the que will replace it.\n**what this this mean for my existing gsi?**\nnothing! i specifically wrote this all in a way that the game state integration itself isnt affected, there are no available updates for the gsi.\n**does this mean you can see all my data?**\nof course not! the only non-cs related thing i can view is your steamid64 (which you can find by going to someones profile)\n**ok but why should i care?**\nthe round data being collected from this (rounds won/lost, equipment cost, etc) is being fed into a maching learning algorithm that tries to calculate the chance that a team will win a round. even though csgo already does this, all it does is divide the equipment costs of both teams, while this accounts for how many rounds this team previously won with a similar equipment cost.\n**how do i join?**\nits really easy, just go to https://github.com/realjoerogan/rogangsi and click the green code button and press download zip. unzip it using 7zip or any alternatives and put .cfg file located in the unzipped folder into the cfg directory of your cs installation.",
+    #     timestamp=datetime.now().astimezone(pytz.timezone('US/Eastern'))
+    # )
+    # await channel.send(embed=embed)
     for guild in bot.guilds:
         await guild.me.edit(nick=f"")
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="No Active Matches"))
@@ -58,70 +71,82 @@ async def on_ready():
 
 @tasks.loop(seconds=3)
 async def gsi():
+    global roundEnd
     global matchEnd
     global t_name2
     global ct_name2
 
-    x = requests.get('http://127.0.0.1:3000')
+    x = requests.get('http://127.0.0.1:8792')
     if x.status_code == 200:
-        data = x.json()
-        if "map" in data.keys():
-            ct_score = data['map']['team_ct']['score']
-            t_score = data['map']['team_t']['score']
-            if "name" in data['map']['team_ct'].keys():
-                ct_name = data['map']['team_ct']['name']
-            else:
-                ct_name = "CT's"
-            if "name" in data['map']['team_t'].keys():
-                t_name = data['map']['team_t']['name']
-            else:
-                t_name = "T's"
-            map_name = data['map']['name']
-            for guild in bot.guilds:
-                if "round" in data.keys():
-                    if "bomb" in data['round'].keys():
-                        if data['round']['bomb'] == "planted":
-                            if guild.me.nick != f"bomb planted on {map_name}":
-                                await guild.me.edit(nick=f"bomb planted on {map_name}")
-                        if data['round']['bomb'] == "defused":
-                            if guild.me.nick != f"bomb defused on {map_name}":
-                                await guild.me.edit(nick=f"bomb defused on {map_name}")
-                        if data['round']['bomb'] == "exploded":
-                            if guild.me.nick != f"bomb exploded on {map_name}":
-                                await guild.me.edit(nick=f"bomb exploded on {map_name}")
+        payload = x.json()
+        sorted_data = []
+        provider_messages = []
+
+        # Pick some provider from the list...
+        for providerid in payload.keys():
+            sorted_data.append(payload[providerid])
+
+        # sorted_data = sorted(sorted_data, key=lambda z: (z["provider"]["steamid"]))
+        if len(sorted_data) > 0:
+            provider_messages = sorted_data[0]
+
+        if len(provider_messages) > 0:
+            data = provider_messages[len(provider_messages) - 1]  # Get last (the most recent array element)
+            if "map" in data.keys():
+                ct_score = data['map']['team_ct']['score']
+                t_score = data['map']['team_t']['score']
+                if "name" in data['map']['team_ct'].keys():
+                    ct_name = data['map']['team_ct']['name']
                 else:
-                    if guild.me.nick != f"live on {map_name}":
-                        await guild.me.edit(nick=f"live on {map_name}")
-            await bot.change_presence(activity=discord.Game(name=f"{ct_name} {ct_score} - {t_score} {t_name}"))
-            if "phase" in data['map'].keys():
-                gamePhase = data['map']['phase']
-                if gamePhase == "gameover":
-                    if matchEnd == 0:
-                        matchEnd = 1
-                        print("game over")
-                        for guild in bot.guilds:
-                            scoreChannel = discord.utils.get(guild.channels, name="match-results")
-                            if not scoreChannel:
-                                scoreChannel = await guild.create_text_channel(name="match-results")
-                            channel = bot.get_channel(scoreChannel.id)
-                            embed = discord.Embed(
-                                title=f"{ct_name.replace(ct_name2, 'counter terrorists')} {ct_score} - {t_score} {t_name.replace(t_name2, 'prob terrorists idk')}",
-                                timestamp=datetime.now().astimezone(pytz.timezone('US/Eastern'))
-                            )
-                            await channel.send(embed=embed)
+                    ct_name = "CT's"
 
+                if "name" in data['map']['team_t'].keys():
+                    t_name = data['map']['team_t']['name']
+                else:
+                    t_name = "T's"
+                map_name = data['map']['name']
 
-        else:
-            print("no map")
-            matchEnd = 0
+                print(f"Processing data for {data['provider']['steamid']} : {map_name} {ct_score} - {t_score}")
+                for guild in bot.guilds:
+                    is_workshop_name = map_name.split("/")
+                    if len(is_workshop_name) > 1:
+                        new_name = is_workshop_name[2]
+                    else:
+                        new_name = map_name
+                    if data['map']['phase'] == "live":
+                        if guild.me.nick != f"live on {new_name}":
+                            await guild.me.edit(nick=f"live on {new_name}")
+                    if data['map']['phase'] == "warmup":
+                        if guild.me.nick != f"warmup on {new_name}":
+                            await guild.me.edit(nick=f"warmup on {new_name}")
+                await bot.change_presence(activity=discord.Game(name=f"{ct_name} {ct_score} - {t_score} {t_name}"))
+                if "phase" in data['map'].keys():
+                    gamePhase = data['map']['phase']
+                    if gamePhase == "gameover":
+                        if matchEnd == 0:
+                            matchEnd = 1
+                            print("game over")
+                            for guild in bot.guilds:
+                                scoreChannel = discord.utils.get(guild.channels, name="match-results")
+                                if not scoreChannel:
+                                    scoreChannel = await guild.create_text_channel(name="match-results")
+                                channel = bot.get_channel(scoreChannel.id)
+                                embed = discord.Embed(
+                                    title=f"{ct_name.replace(ct_name2, 'counter terrorists')} {ct_score} - {t_score} {t_name.replace(t_name2, 'prob terrorists idk')}",
+                                    timestamp=datetime.now().astimezone(pytz.timezone('US/Eastern'))
+                                )
+                                await channel.send(embed=embed)
+            else:
+                print("no map")
+                matchEnd = 0
+                await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=("No Active Matches")))
     else:
         print("no map")
         for guild in bot.guilds:
-            if guild.me.nick != (f""):
-                await guild.me.edit(nick=f"")
+            if guild.me.nick != "":
+                await guild.me.edit(nick="")
         await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=("No Active Matches")))
         matchEnd = 0
-
 
 
 @bot.command()
@@ -140,34 +165,6 @@ async def r(ctx, index):
     song = await player.remove_from_queue(int(index))
     await ctx.send(f'{song.name} was removed from the queue lmao bald')
 
-@bot.command()
-async def rps(ctx, symbol):
-
-    global turn
-    global symbol1
-    global symbol2
-    global name1
-    global name2
-
-    if turn == 0:
-        symbol1 = (ctx.message.content[5:])
-        name1 = (ctx.message.author.display_name)
-        await ctx.message.delete()
-        turn = 1
-
-    if (turn == 1):
-        symbol2 = (ctx.message.content[5:])
-        name2 = (ctx.message.author.display_name)
-        await ctx.message.delete()
-        embed = discord.Embed(
-            title=f"{name1} vs {name2}",
-            description=f"{name1} selected: **{symbol1}** \n {name2} selected: **{symbol2}**",
-            # timestamp=datetime.now().astimezone(pytz.timezone('US/Eastern'))
-        )
-        # embed.set_footer(text=f"Hash: {ctx.message.__hash__}")
-        # embed.set_footer(text=f"Hash: {ctx.message.__hash__}")
-        await ctx.send(embed=embed)
-        turn = 0
 
 @bot.command()
 async def p(ctx, *, url):
@@ -210,57 +207,6 @@ async def np(ctx):
     player = music.get_player(guild_id=ctx.guild.id)
     song = player.now_playing()
     await ctx.send(song.name)
-
-@bot.command()
-@commands.has_permissions(manage_messages=True)
-async def score(ctx, score1=None, score2=None, team1=None, team2=None):
-    guild = ctx.guild
-    embed = discord.Embed(
-        title=f"{team1} {score1} - {score2} {team2}",
-        timestamp=datetime.now().astimezone(pytz.timezone('US/Eastern'))
-    )
-    channel = discord.utils.get(ctx.guild.channels, name="Match Results")
-    if not channel:
-        channel = await guild.create_text_channel(name="Match Results")
-    await channel.send(embed=embed)
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=(f"{team1} {score1} - {score2} {team2}")))
-
-
-@bot.command()
-@commands.has_permissions(manage_messages=True)
-async def tempmute(ctx, member: discord.Member, timed: DurationConverter, *, reason=None):
-    guild = ctx.guild
-    mutedRole = discord.utils.get(guild.roles, name="Muted")
-
-    if not mutedRole:
-        mutedRole = await guild.create_role(name="Muted")
-
-        for channel in guild.channels:
-            await channel.set_permissions(mutedRole, speak=False, send_messages=False, read_message_history=True, read_messages=True)
-
-    multiplier = {'s': 1, 'm': 60, 'h': 3600}
-    amount, unit = timed
-
-    await member.add_roles(mutedRole, reason=None)
-    embed = discord.Embed(
-        title="Member Muted",
-        description=(f"Member {member.mention} was muted for {amount}{unit}, reason {reason}."),
-        timestamp=datetime.now().astimezone(pytz.timezone('US/Eastern'))
-    )
-    await ctx.send(embed=embed)
-
-    mute_time = amount * multiplier[unit]
-    print(mute_time)
-    await asyncio.sleep(amount * multiplier[unit])
-    print("User Unmuted")
-
-    await member.remove_roles(mutedRole)
-    embed = discord.Embed(
-        title="Member Unmuted",
-        description=(f"Member {member.mention} has been unmuted, mute reason '{reason}.'"),
-        timestamp=datetime.now().astimezone(pytz.timezone('US/Eastern'))
-    )
-    await ctx.send(embed=embed)
-
+    
 bot.run(token)
 
